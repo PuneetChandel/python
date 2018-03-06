c5c780cb813c9e84d3b5e6b9c5e32ebc704bdf49
Puneet Chandel <puneet.chandel@gmail.com>
2018-03-05 20:31:01 -0600

    Create maxsublist.py

---
+++ maxsublist.py
@@ -0,0 +1,27 @@
+# Given an array of integers, find and print the maximum number of integers you can select from the array such that 
+# the absolute difference between any two of the chosen integers is <=1
+
+import sys
+
+def pickingNumbers(a):
+    arr1=[]
+    result=[]
+    a.sort()    
+    for x in range(0,len(a)):
+        for y in range(x,len(a)):
+            if abs(a[x]-a[y])<=1:
+                if len(arr1)>0:
+                    if abs(min(arr1)-a[y])<=1:
+                         arr1.append(a[y])
+                else:
+                    arr1.append(a[y])    
+        
+        if len(arr1)>len(result) or x==0:
+            result=arr1
+        arr1=[]
+    return len(result)    
+if __name__ == "__main__":
+    n = int(input().strip())
+    a = list(map(int, input().strip().split(' ')))
+    result = pickingNumbers(a)
+    print(result)
